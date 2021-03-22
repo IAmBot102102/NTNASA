154ed5c7527cba966b90c2bc6aafb4cab915e092
noah <betajsion@gmail.com>
2021-03-22 06:17:53 -0400

    worked on front end horide but a start

---
+++ Client Monitor/index.js
@@ -0,0 +1,7 @@
+document.onload = function(){
+    //Setting width+height of box
+    var root_box = document.getElementById('root');
+    root_box.width = document.innerWidth;
+
+    // global-scope varibles
+}
